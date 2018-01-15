5d81bba10250194371ff277c04e3f6a0c18beb75
Shannon Prescott <shan.prescott@outlook.com>
2018-01-15 18:24:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ function whisper(string) {
 }
 whisper()
 
-function sayHitoGrandma(string) {
+function logShout(string) {
+  console.log(shout(string))
+}
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
+function sayHiToGrandma(string) {
+  if (string === whisper(string)) {
+    return "I can't hear you!"
+  }
   
+  else if (string === shout(string)) {
+    return "YES INDEED!"
+  }
+  
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
